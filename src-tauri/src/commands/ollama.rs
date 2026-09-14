79715dae6e70b3cf8ebbd51b3bebad930f800239
Amen974 <aamenallah593@email.com>
2026-09-15 00:45:09 +0100

    feat: add setion history

--- src-tauri/src/commands/ollama.rs
+++ src-tauri/src/commands/ollama.rs
@@ -1,16 +1,26 @@
 use futures_util::StreamExt;
-use tauri::{AppHandle, Emitter};
+use tokio::sync::Mutex;
+use tauri::{AppHandle, Emitter, State};
+
+use crate::{Message, MessageState, Role::{ Assistant, User}};
 
 #[derive(serde::Serialize)]
+#[derive(Clone)]
 struct OllamaRequest {
     model: String,
-    prompt: String,
+    messages: Vec<Message>,
     stream: bool,
 }
 
 #[derive(serde::Deserialize)]
+#[derive(Debug)]
+struct OllamaMessage {
+    content: String,
+}
+
+#[derive(serde::Deserialize)]
 struct OllamaResponse {
-    response: Option<String>,
+    message: Option<OllamaMessage>,
     done: bool,
 }
 
@@ -18,17 +28,29 @@ struct OllamaResponse {
 pub async fn send_message(
     app: AppHandle,
     message: String,
+    message_arr: State<'_, Mutex<MessageState>>,
 ) -> Result<(), String> {
     let client = reqwest::Client::new();
 
+    let messages = {
+        let mut state = message_arr.lock().await;
+
+        state.messages.push(Message {
+            role: User.as_str(),
+            content: message,
+        });
+
+        state.messages.clone()
+    };
+
     let payload = OllamaRequest {
         model: "qwen3:4b".to_string(),
-        prompt: message,
+        messages,
         stream: true,
     };
 
     let response = client
-        .post("http://localhost:11434/api/generate")
+        .post("http://localhost:11434/api/chat")
         .json(&payload)
         .send()
         .await
@@ -43,9 +65,14 @@ pub async fn send_message(
 
     let mut stream = response.bytes_stream();
     let mut buffer = String::new();
+    let mut full_message = String::new();
 
     while let Some(chunk) = stream.next().await {
         let chunk = chunk.map_err(|error| error.to_string())?;
+
+        let text = String::from_utf8_lossy(&chunk);
+        println!("RAW CHUNK: {}", text);
+
         buffer.push_str(&String::from_utf8_lossy(&chunk));
 
         while let Some(pos) = buffer.find('\n') {
@@ -56,15 +83,27 @@ pub async fn send_message(
                 continue;
             }
 
-            let data: OllamaResponse = 
+            let data: OllamaResponse =
                 serde_json::from_str(&line).map_err(|error| error.to_string())?;
+            println!("PARSED: {:?}", line);
+            println!("AI RESPONSE: {:?}", data.message);
+            println!("DONE: {}", data.done);
 
-            if let Some(response) = data.response {
-                app.emit("chat-chunk", response)
+            if let Some(message) = data.message {
+                full_message.push_str(&message.content);
+
+                app.emit("chat-chunk", message.content)
                     .map_err(|error| error.to_string())?;
             }
 
             if data.done {
+                let mut state = message_arr.lock().await;
+
+                state.messages.push(Message {
+                    role: Assistant.as_str(),
+                    content: full_message,
+                });
+
                 app.emit("chat-done", ())
                     .map_err(|error| error.to_string())?;
 
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,41 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tokio::sync::Mutex;
+use crate::commands::ollama::send_message;
 
 mod commands;
 
-use crate::commands::ollama::send_message;
+struct MessageState {
+    messages: Vec<Message>,
+}
+
+#[derive(serde::Serialize)]
+#[derive(Clone)]
+struct Message {
+    role: &'static str,
+    content: String,
+}
+
+#[derive(serde::Serialize)]
+#[derive(Clone)]
+enum Role {
+    User,
+    Assistant,
+}
+
+impl Role {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Role::User => "user",
+            Role::Assistant => "assistant",
+        }
+    }
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(Mutex::new(MessageState {
+            messages: Vec::new(),
+        }))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![send_message])
         .run(tauri::generate_context!())
